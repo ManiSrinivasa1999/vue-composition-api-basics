c8416fafdcaf0f31454f4bed51ec1a71db6f2f21
BADAM MANISRINIVASA NARAYANA <50072053+ManiSrinivasa1999@users.noreply.github.com>
2022-06-08 09:07:58 +0530

    add code for hooks,watchers

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
   <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
   </nav>
-
-  <RouterView />
+  <router-view />
 </template>
 
 <style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,13 @@
     <h2>{{ appTitle }}</h2>
     <h3>{{ counterData.title }}:</h3>
     <div>
-      <button class="btn" @click="decreaseCounter">-</button>
+      <button class="btn" @click="decreaseCounter(2)">--</button>
+      <button class="btn" @click="decreaseCounter(1)">-</button>
       <span class="counter">{{ counterData.count }}</span>
-      <button class="btn" @click="increaseCounter">+</button>
+      <button class="btn" @click="increaseCounter(1, $event)">+</button>
+      <button class="btn" @click="increaseCounter(2)">++</button>
     </div>
+    <p>This counter is {{ oddOrEven }}</p>
     <div class="edit">
       <h4>Edit counter title:</h4>
       <input v-model="counterData.title" type="text" />
@@ -15,21 +18,81 @@
 </template>
 
 <script setup>
-import { reactive } from '@vue/reactivity'
+import {
+  reactive,
+  computed,
+  watch,
+  onBeforeMount,
+  onBeforeUnmount,
+  onMounted,
+  onUnmounted,
+  onActivated,
+  onDeactivated,
+  onBeforeUpdate,
+  onUpdated,
+} from 'vue'
 
 const appTitle = 'My Amazing Counter App'
+onMounted(() => {
+  console.log('apptitle')
+})
 
 const counterData = reactive({
   count: 0,
   title: 'My Counter',
 })
 
-const increaseCounter = () => {
-  counterData.count += 1
+// watcher
+watch(
+  () => counterData.count,
+  (newCount) => {
+    if (newCount == 20) {
+      alert('You reached 20')
+    }
+  }
+)
+
+// computed property
+const oddOrEven = computed(() => {
+  return counterData.count % 2 ? 'odd' : 'even'
+})
+
+// methods or functions
+const increaseCounter = (amount, e) => {
+  counterData.count += amount
 }
-const decreaseCounter = () => {
-  counterData.count -= 1
+const decreaseCounter = (amount) => {
+  counterData.count -= amount
 }
+onMounted(() => {
+  console.log('counter')
+})
+
+// life cycle hooks
+onBeforeUpdate(() => {
+  console.log('onBeforeUpdate')
+})
+onUpdated(() => {
+  console.log('onUpdated')
+})
+onBeforeMount(() => {
+  console.log('on before mount')
+})
+onMounted(() => {
+  console.log('on mount')
+})
+onBeforeUnmount(() => {
+  console.log('on before unmount')
+})
+onUnmounted(() => {
+  console.log('on unmounted')
+})
+onActivated(() => {
+  console.log('on activated')
+})
+onDeactivated(() => {
+  console.log('on deactivated')
+})
 </script>
 
 <style>
@@ -46,41 +109,3 @@ const decreaseCounter = () => {
   margin-top: 60px;
 }
 </style>
-
-<!-- 
-<script>
-import { ref } from '@vue/reactivity'
-export default {
-  setup() {
-    const counter = ref(0)
-    const increaseCounter = () => {
-      counter.value += 1
-    }
-    const decreaseCounter = () => {
-      counter.value -= 1
-    }
-
-    return { counter, increaseCounter, decreaseCounter }
-  },
-}
-</script>
-
-
-<script>
-export default {
-  data() {
-    return {
-      counter: 0,
-    };
-  },
-  methods: {
-    increaseCounter() {
-      this.counter += 1;
-    },
-    decreaseCounter() {
-      this.counter -= 1;
-    },
-  },
-};
-</script>
--->
